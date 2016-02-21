6308f38083342e38d9b3845ca1f5fe396bc044ec
Jacob Sanford <jsanford@unb.ca>
2016-02-21 19:33:36 -0400

    Introduce development environment switch to pipe logs and errors to docker journal

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,13 @@ ENV APP_HOSTNAME nginx.local
 ENV APP_ROOT /app
 ENV NGINX_RUN_GROUP nginx
 ENV NGINX_RUN_USER nginx
+ENV DEPLOY_ENV prod
 
 ENV APP_LOG_DIR $APP_ROOT/log
 ENV APP_WEBROOT $APP_ROOT/html
+
+ENV NGINX_LOG_FILE ${APP_LOG_DIR}/${APP_HOSTNAME}.access.log
+ENV NGINX_ERROR_LOG_FILE ${APP_LOG_DIR}/${APP_HOSTNAME}.error.log
 
 RUN apk --update add nginx && \
   rm -f /var/cache/apk/* && \
--- scripts/pre-init.d/10_nginx_conf.sh
+++ scripts/pre-init.d/10_nginx_conf.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+if [ "$DEPLOY_ENV" = "dev" ]; then
+  sed -i "s|NGINX_LOG_FILE|/proc/self/fd/2|g" /etc/nginx/conf.d/app.conf
+  sed -i "s|NGINX_ERROR_LOG_FILE|/proc/self/fd/2|g" /etc/nginx/conf.d/app.conf
+else
+  sed -i "s|NGINX_LOG_FILE|$NGINX_LOG_FILE|g" /etc/nginx/conf.d/app.conf
+  sed -i "s|NGINX_ERROR_LOG_FILE|$NGINX_ERROR_LOG_FILE|g" /etc/nginx/conf.d/app.conf
+fi
+
 sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/nginx/conf.d/app.conf
-sed -i "s|APP_LOG_DIR|$APP_LOG_DIR|g" /etc/nginx/conf.d/app.conf
 sed -i "s|APP_WEBROOT|$APP_WEBROOT|g" /etc/nginx/conf.d/app.conf
 sed -i "s|NGINX_RUN_USER|$NGINX_RUN_USER|g" /etc/nginx/nginx.conf
