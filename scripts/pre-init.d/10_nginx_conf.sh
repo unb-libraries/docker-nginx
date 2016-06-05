b005e62ab8f4043a91d56ef6e9402fc3220709ee
Jacob Sanford <jsanford@unb.ca>
2016-06-04 21:51:47 -0300

    Standardize on app logging to docker log.

--- scripts/pre-init.d/10_nginx_conf.sh
+++ scripts/pre-init.d/10_nginx_conf.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
-if [ "$DEPLOY_ENV" = "dev" ]; then
-  sed -i "s|NGINX_LOG_FILE|/proc/self/fd/2|g" /etc/nginx/conf.d/app.conf
-  sed -i "s|NGINX_ERROR_LOG_FILE|/proc/self/fd/2|g" /etc/nginx/conf.d/app.conf
-else
-  sed -i "s|NGINX_LOG_FILE|$NGINX_LOG_FILE|g" /etc/nginx/conf.d/app.conf
-  sed -i "s|NGINX_ERROR_LOG_FILE|$NGINX_ERROR_LOG_FILE|g" /etc/nginx/conf.d/app.conf
-fi
+sed -i "s|NGINX_LOG_FILE|$NGINX_LOG_FILE|g" /etc/nginx/conf.d/app.conf
+sed -i "s|NGINX_ERROR_LOG_FILE|$NGINX_ERROR_LOG_FILE|g" /etc/nginx/conf.d/app.conf
 
 sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/nginx/conf.d/app.conf
 sed -i "s|APP_WEBROOT|$APP_WEBROOT|g" /etc/nginx/conf.d/app.conf
