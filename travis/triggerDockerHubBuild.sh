fb2faefa714e20d08ad7a94d0a177db5a1f1fefb
Jacob Sanford <jsanford@unb.ca>
2016-06-05 11:15:49 -0300

    Further generalization and standardization

--- travis/triggerDockerHubBuild.sh
+++ travis/triggerDockerHubBuild.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# Trigger an image's build on dockerhub. DOCKER_HUB_TRIGGER_TOKEN must be set
+# in the Repository's "Environment Variables" on travis-ci.org.
 curl -H "Content-Type: application/json" --data "{'docker_tag': '${DOCKER_IMAGE_TAG}'}" -X POST "https://registry.hub.docker.com/u/${DOCKER_IMAGE_NAME}/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/"
--- travis/waitForNginxDeploy.sh
+++ travis/waitForNginxDeploy.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Test the connection
 function testNginxPage() {
   curl -I --fail http://127.0.0.1/test.html
   return $?
 }
 
-retry=0
-maxRetries=5
-retryInterval=60
-until [ ${retry} -ge ${maxRetries} ]
+CONNECT_RETRY_COUNT=0
+CONNECT_RETRY_INTERVAL=60
+MAX_CONNECT_RETRIES=5
+ENDPOINT_NAME="Nginx"
+
+until [ ${CONNECT_RETRY_COUNT} -ge ${MAX_CONNECT_RETRIES} ]
 do
   testNginxPage && break
-  retry=$[${retry}+1]
-  echo "Nginx has not deployed. Waiting [${retry}/${maxRetries}] in ${retryInterval}(s) "
-  sleep ${retryInterval}
+  CONNECT_RETRY_COUNT=$[${CONNECT_RETRY_COUNT}+1]
+  echo "${ENDPOINT_NAME} has not deployed. Waiting [${CONNECT_RETRY_COUNT}/${MAX_CONNECT_RETRIES}] in ${CONNECT_RETRY_INTERVAL}(s) "
+  sleep ${CONNECT_RETRY_INTERVAL}
 done
 
-if [ ${retry} -ge ${maxRetries} ]; then
-  echo "Connecting to Nginx failed after ${maxRetries} attempts!"
+if [ ${CONNECT_RETRY_COUNT} -ge ${MAX_CONNECT_RETRIES} ]; then
+  echo "Connecting to ${ENDPOINT_NAME} failed after ${MAX_CONNECT_RETRIES} attempts!"
   exit 1
 fi
