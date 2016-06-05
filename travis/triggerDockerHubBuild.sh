9fa927d9ed7af9c14cf876783c7388e5a06add59
Jacob Sanford <jsanford@unb.ca>
2016-06-05 11:05:26 -0300

    Generalize travis tests

--- travis/triggerDockerHubBuild.sh
+++ travis/triggerDockerHubBuild.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -H "Content-Type: application/json" --data '{"docker_tag": "alpine"}' -X POST https://registry.hub.docker.com/u/unblibraries/nginx-php/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/
+curl -H "Content-Type: application/json" --data "{'docker_tag': '${DOCKER_IMAGE_TAG}'}" -X POST "https://registry.hub.docker.com/u/${DOCKER_IMAGE_NAME}/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/"
