043e0cbc97b5967145421c50d12d5f5ab6a06b2c
Jacob Sanford <jsanford@unb.ca>
2021-06-29 12:53:02 -0300

    Format startup reporting

--- scripts/pre-init.d/99_z_report_completion.sh
+++ scripts/pre-init.d/99_z_report_completion.sh
@@ -6,5 +6,6 @@ STARTUP_TIME=`expr $NOW - $START_TIME`
 echo "-------------------" >> /tmp/deploy_time.txt
 printf "Total : ${STARTUP_TIME}s\n" >> /tmp/deploy_time.txt
 
-printf "\nDeployment Complete.\n-------------------"
+printf "\nDeployment Complete.\n-------------------\n"
 cat /tmp/deploy_time.txt
+printf "\n"
