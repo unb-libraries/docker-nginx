85bc35caa4e3209b61ec8ae36b52324a10689560
Jacob Sanford <jsanford@unb.ca>
2021-06-29 12:29:18 -0300

    Streamline completion notices

--- scripts/pre-init.d/99_z_report_completion.sh
+++ scripts/pre-init.d/99_z_report_completion.sh
@@ -2,6 +2,9 @@
 START_TIME=`cat /tmp/start_time`
 NOW=`date +%s`
 STARTUP_TIME=`expr $NOW - $START_TIME`
+
 echo "-------------------" >> /tmp/deploy_time.txt
-echo "Total : ${STARTUP_TIME}s" >> /tmp/deploy_time.txt
+printf "Total : ${STARTUP_TIME}s\n" >> /tmp/deploy_time.txt
+
+printf "\nDeployment Complete.\n-------------------"
 cat /tmp/deploy_time.txt
